1d4a9c637643f36130ad08153620ec2e02b8c9a5
Gustavo Adami <gustavo_adami@hotmail.com>
2019-03-21 12:10:30 -0400

    NYTimes branch created. Class, layout and string files renamed

--- app/src/main/java/com/example/androidfinalproject/NYTimes_MainActivity.java
+++ app/src/main/java/com/example/androidfinalproject/NYTimes_MainActivity.java
@@ -3,13 +3,12 @@ package com.example.androidfinalproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class NYTimes_MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.nytimes_activity_main);
 
-        // Buttons for apps
     }
 }
