211d5c2f8ce5dff7b88257cb61dcfcac26435d4e
Gustavo Adami <gustavo_adami@hotmail.com>
2019-03-21 12:12:38 -0400

    FlightStatus branch created. Class, layout and string files renamed

---
+++ app/src/main/java/com/example/androidfinalproject/FlightStatus_MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.androidfinalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FlightStatus_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.flightstatus_activity_main);
+
+        // Buttons for apps
+    }
+}
