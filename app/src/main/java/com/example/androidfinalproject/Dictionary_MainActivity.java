fe55e96c1d74aa8749e642f94e34de896b0ba78f
Gustavo Adami <gustavo_adami@hotmail.com>
2019-03-21 11:45:14 -0400

    Dictionary initial branch created

--- app/src/main/java/com/example/androidfinalproject/Dictionary_MainActivity.java
+++ app/src/main/java/com/example/androidfinalproject/Dictionary_MainActivity.java
@@ -3,12 +3,12 @@ package com.example.androidfinalproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Dictionary_MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.dictionary_activity_main);
 
         // Buttons for apps
     }
