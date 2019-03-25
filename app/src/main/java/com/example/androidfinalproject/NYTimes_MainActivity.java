a9e1f15b5ed88965b657ac6427d8ff50d1aa36a4
Rodrigo Eltz <rpetrye@hotmail.com>
2019-03-25 14:53:57 -0400

    Mar 25,Rodrigo

--- app/src/main/java/com/example/androidfinalproject/NYTimes_MainActivity.java
+++ app/src/main/java/com/example/androidfinalproject/NYTimes_MainActivity.java
@@ -2,13 +2,34 @@ package com.example.androidfinalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.support.design.widget.Snackbar;
 
 public class NYTimes_MainActivity extends AppCompatActivity {
+    private Button goBack;
+    private Button search;
+    private EditText typeSearch;
+    private ListView nyFeed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nytimes_activity_main);
+        goBack = findViewById(R.id.nyBackButton);
+        typeSearch = findViewById(R.id.nyTypeSearch);
+        search = findViewById(R.id.nySearchButton);
+        nyFeed = findViewById(R.id.listView);
+
+
+        goBack.setOnClickListener(a -> {
+            Snackbar sb = Snackbar.make(goBack, "Go Back?", Snackbar.LENGTH_LONG);
+            sb.setAction("Confirm!", b -> finish());
+            sb.show();
+        });
+
 
     }
 }
---
+++ app/src/main/java/com/example/androidfinalproject/Nytimes_home.java
@@ -0,0 +1,13 @@
+package com.example.androidfinalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Nytimes_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nytimes_home);
+    }
+}
